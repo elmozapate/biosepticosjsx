aa41c75dff9d311b8814740d057f30e6ca7df591
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-01-25 20:19:13 -0500

    fix

--- src/pages/meliPage/index.jsx
+++ src/pages/meliPage/index.jsx
@@ -1,22 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import MeliText from './melitext'
 
-
+let index = 0
 export default function Home() {
     const mensajes = ['Hola Meli', 'Sabes algo....', 'Lo que te digo no es mentira, no quiero ser otro contigo', 'Me haces mucho bien', 'ademas....', 'te lo confesare', 'Me gustas mucho', 'te quiero en mi vida, como amiga  siempre', 'y ojala tambien como amantes, lo demas dejemoslo al destino', 'pase muy rico en amaga', ' Quieres que te haga un Blog, asi el mundo sabra lo mamacita que eres XD']
     const [usersArray, setUsersArray] = useState(0)
 
-    setInterval(() => {
-        if (mensajes.length > usersArray+1) {
-            const index = usersArray
+    useEffect(() => {
+        setInterval(() => {
+            if (mensajes.length > index + 1) {
+                index = index + 1
+                setUsersArray(index)
+            } else {
+                setUsersArray(-1)
+            }
 
-            setUsersArray(index + 1)
-        }else{
-            setUsersArray(-1)
-        }
+        }, 5000);
+    }, [])
 
-    }, 5000);
     return (
         <>
             <Head>
@@ -26,12 +29,13 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={'div-main'}>
-                <div className='melipage'>
+                <MeliText usersArray={usersArray} mensajes={mensajes} />
+                {/*    <div className='melipage'>
                     {
-                        usersArray!==-1?                    <p>{mensajes[usersArray]}</p>
-                        :<p>Mamacita MUAKSS</p>
+                        usersArray !== -1 ? <p>{mensajes[usersArray]}</p>
+                            : <p>Mamacita MUAKSS</p>
                     }
-                </div>
+                </div> */}
 
             </main>
         </>
